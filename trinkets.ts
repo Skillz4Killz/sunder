692d6d236214d4998583d0271ba493fafb758dd8
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-06-08 19:17:02 -0400

    Create trinkets.ts

---
+++ trinkets.ts
@@ -0,0 +1,330 @@
+export default [
+  {
+    name: "Belt of Phasing",
+    description: "Doding sometimes isn't enough.",
+    quote: "Instantly gain a shield upon activating your dash.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Health Max", value: "40" },
+          { name: "Shield Duration", value: "1s" },
+          { name: "Shield Strength", value: "225" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Health Max", value: "50.8" },
+          { name: "Shield Duration", value: "1.01s" },
+          { name: "Shield Strength", value: "230" },
+        ],
+      },
+    ],
+    banner: "Protector",
+  },
+  {
+    name: "Ring of the Veil",
+    description: "Gain primal energy upon defeating a human or primal enemy.",
+    quote: "This world is not what it ought to be.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Health Max", value: "100" },
+          { name: "Primal Charge", value: "125%" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Health Max", value: "112" },
+          { name: "Primal Charge", value: "127.5%" },
+        ],
+      },
+    ],
+    banner: "Assassin",
+  },
+  {
+    name: "Hafthor's Gauntlet",
+    description: "Take reduced damage from primals.",
+    quote: "Reject your sense of injury and the injury itself will disappear.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Health Max", value: "60" },
+          { name: "Primal Damage Reduction", value: "40%" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Health Max", value: "71.2" },
+          { name: "Primal Damage Reduction", value: "41%" },
+        ],
+      },
+    ],
+    banner: "Primal Hunter",
+  },
+  {
+    name: "Rylan's Grace",
+    description: "Activating your combat roll from brush will periodically not expend its cooldown.",
+    quote: "Some things are truly free.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Health Max", value: "80" },
+          { name: "Effect Cooldown", value: "4s" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Health Max", value: "91.6" },
+          { name: "Effect Cooldown", value: "3.85s" },
+        ],
+      },
+    ],
+    banner: "Ranger",
+  },
+  {
+    name: "Warborn Amulet",
+    description: "Instantly gain health upon firing your heavy weapon.",
+    quote: "War is a force that gives you meaning.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Health Max", value: "80" },
+          { name: "Heal Strength", value: "175" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Health Max", value: "91.6" },
+          { name: "Heal Strength", value: "179" },
+        ],
+      },
+    ],
+    banner: "Primal Hunter",
+  },
+  {
+    name: "Empathy Band",
+    description: "You and nearby allies gain a shield based on your max health at the end of your dash.",
+    quote: "If you want to lead, you'll have to serve.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Health Max", value: "40" },
+          { name: "Health to Shield Ratio", value: "6%" },
+          { name: "Shield Duration", value: "1s" },
+          { name: "Effect Radius", value: "5.2" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Health Max", value: "50.8" },
+          { name: "Health to Shield Ratio", value: "6.1%" },
+          { name: "Shield Duration", value: "1.01s" },
+          { name: "Effect Radius", value: "5.2" },
+        ],
+      },
+    ],
+    banner: "Protector",
+  },
+  {
+    name: "Reckful Circlet",
+    description: "Gain increased movespeed after staying in brush for a duration.",
+    quote: "Once worn by the world's greatest rogue.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Health Max", value: "60" },
+          { name: "Delay Before Trigger", value: "4s" },
+          { name: "Movespeed Duration", value: "2.4s" },
+          { name: "Movespeed Strength", value: "50%" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Health Max", value: "71.2" },
+          { name: "Delay Before Trigger", value: "3.95s" },
+          { name: "Movespeed Duration", value: "2.43s" },
+          { name: "Movespeed Strength", value: "50.25%" },
+        ],
+      },
+    ],
+    banner: "Assassin",
+  },
+  {
+    name: "Exhortation Talisman",
+    description: "You and nearby allies gain increased damage at the end of your dash.",
+    quote: "There is an opportunity to be found in every difficulty.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Health Max", value: "80" },
+          { name: "Bonus Damage Duration", value: "1.8s" },
+          { name: "Bonus Damage Strength", value: "5%" },
+          { name: "Effect Radius", value: "4.2" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Health Max", value: "91.6" },
+          { name: "Bonus Damage Duration", value: "1.83s" },
+          { name: "Bonus Damage Strength", value: "5.1%" },
+          { name: "Effect Radius", value: "4.2" },
+        ],
+      },
+    ],
+    banner: "Support",
+  },
+  {
+    name: "Bracelet of the Firstborn",
+    description: "You and your allies gain a shield based on your max health when you activate your primal ability.",
+    quote: "Unmerited favor beyond your comprehension.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Health Max", value: "120" },
+          { name: "Health to Shield Ratio", value: "50%" },
+          { name: "Shield Duration", value: "2.8s" },
+          { name: "Effect Radius", value: "6.8" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Health Max", value: "132" },
+          { name: "Health to Shield Ratio", value: "50.75%" },
+          { name: "Shield Duration", value: "2.83s" },
+          { name: "Effect Radius", value: "6.8" },
+        ],
+      },
+    ],
+    banner: "Support",
+  },
+  {
+    name: "Karin's Cache",
+    description: "Gain increased charge when picking up heavy ammo and primal energy.",
+    quote: "This satchel radiates an aura of unbridled aggression.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Health Max", value: "140" },
+          { name: "Effect Cooldown", value: "45%" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Health Max", value: "153" },
+          { name: "Effect Cooldown", value: "46%" },
+        ],
+      },
+    ],
+    banner: "Seeker",
+  },
+  {
+    name: "Bracer of Dedication",
+    description: "Gain heavy ammo upon defeating a human or primal enemy.",
+    quote: "Your biggest risk is found in never taking any.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Health Max", value: "60" },
+          { name: "Heavy Ammo", value: "80%" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Health Max", value: "71.2" },
+          { name: "Heavy Ammo", value: "82%" },
+        ],
+      },
+    ],
+    banner: "Sniper",
+  },
+  {
+    name: "Swiftstrike Pendant",
+    description: "Gain increased movespeed upon defeating a human or primal enemy.",
+    quote: "On the move--no trace--only the next target.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Health Max", value: "40" },
+          { name: "Movespeed Duration", value: "2s" },
+          { name: "Movespeed Strength", value: "50%" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Health Max", value: "50.8" },
+          { name: "Movespeed Duration", value: "2.04s" },
+          { name: "Movespeed Strength", value: "51%" },
+        ],
+      },
+    ],
+    banner: "Sniper",
+  },
+  {
+    name: "Nihilum Guard",
+    description: "Deal increased damage in both human and primal forms.",
+    quote: "Anything less than the best is a buzzkill.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Health Max", value: "83.2" },
+          { name: "All Damage", value: "11%" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Health Max", value: "94" },
+          { name: "All Damage", value: "11.2%" },
+        ],
+      },
+    ],
+    banner: "Primalist",
+  },
+  {
+    name: "Evasion Loop",
+    description: "Temporarily gain a bonus dash charge upon defeating a human or primal enemy.",
+    quote: "The best defense is a good offense.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Health Max", value: "140" },
+          { name: "Bonus Duration", value: "10%" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Health Max", value: "153" },
+          { name: "Bonus Duration", value: "10.4%" },
+        ],
+      },
+    ],
+    banner: "Vanguard",
+  },
+]
