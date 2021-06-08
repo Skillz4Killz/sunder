f18d33cc04168a1f45bdf05003d74e21d49d28cd
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-06-07 23:11:16 -0400

    Create banners.ts

---
+++ banners.ts
@@ -0,0 +1,47 @@
+export default [
+  {
+    name: "Primal Hunter",
+    description: "Deal increased damage to primal enemies.",
+    stats: ["+25% damage", "+75% damage", "+100% damage", "+125% damage"],
+  },
+  {
+    name: "Vanguard",
+    description: "Gain health upon dealing damage to an enemy.",
+    stats: ["+8 health per hit", "+24 health per hit", "+32 health per hit", "+40 health per hit"],
+  },
+  {
+    name: "Support",
+    description: "Your ability cooldown is reduced.",
+    stats: ["+7% cooldown reduction", "+21% cooldown reduction", "+28% cooldown reduction", "+35% cooldown reduction"],
+  },
+  {
+    name: "Protector",
+    description: "Your maximum health is increased.",
+    stats: ["+12% health", "+36% health", "+48% health", "+60% health"],
+  },
+  {
+    name: "Seeker",
+    description: "Gain increased amounts when picking up heavy ammo and primal energy.",
+    stats: ["+15% charge", "+45% charge", "+60% charge", "+75% charge"],
+  },
+  {
+    name: "Primalist",
+    description: "Your primal cooldown is reduced.",
+    stats: ["+10% charge reduction", "+30% charge reduction", "+40% charge reduction", "+50% charge reduction"],
+  },
+  {
+    name: "Sniper",
+    description: "Deal increased damage while there are no enemies within 8.5m.",
+    stats: ["+5% damage", "+15% damage", "+20% damage", "+25% damage"],
+  },
+  {
+    name: "Assassin",
+    description: "Deal increased damage while within 5.0m of an enemy.",
+    stats: ["+8% damage", "+24% damage", "+32% damage", "+40% damage"],
+  },
+  {
+    name: "Ranger",
+    description: "Gain increased movespeed in brush.",
+    stats: ["+15% movespeed", "+45% movespeed", "+60% movespeed", "+75% movespeed"],
+  },
+]
