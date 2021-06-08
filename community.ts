a3f71ba8a1d1de91fe6e0bb3aaf57e00e7d4895c
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-06-07 23:12:24 -0400

    Create community.ts

---
+++ community.ts
@@ -0,0 +1,44 @@
+export default [
+  {
+    link: "https://www.youtube.com/watch?v=vybaURzEyZ0",
+    image: "https://i.imgur.com/zCXK5FW.jpg",
+    title: "Catalyst Black Early Access Gameplay Don't Spawn Camp It's Whack - ISOROD",
+    date: "September 27, 2020",
+  },
+  {
+    link: "https://www.reddit.com/r/CatalystBlack/comments/j0saa3/spending_20000_dust_in_catalyst_black/",
+    image: "https://i.imgur.com/zdSFkHk.jpg",
+    title: "SPENDING 20,000 Dust in Catalyst Black! - CHAMU",
+    date: "September 27, 2020",
+  },
+  {
+    link: "https://discordapp.com/channels/647509637821628422/671806196562395156/759440976954785853",
+    image: "https://i.imgur.com/58isfEf.jpg",
+    title: "CATALYST BLACK API UI DESIGN - Gjentix",
+    date: "September 26, 2020",
+  },
+  {
+    link: "https://youtu.be/5O0FeRwLAmo",
+    image: "https://media.discordapp.net/attachments/756655002553942157/759853017016172544/image0.jpg",
+    title: "Catalyst Black Soft Thunder + Grounding Bolt OP build -	FLACO",
+    date: "September 25, 2020",
+  },
+  {
+    link: "https://www.reddit.com/r/CatalystBlack/comments/iype5i/cb_is_on_the_google_play_store_in_the_philippines/",
+    image: "https://i.redd.it/nug7fny1l0p51.png",
+    title: "CB is on the Google Play Store in the Philippines for Early Access - u/vgskye",
+    date: "September 24, 2020",
+  },
+  {
+    link: "https://youtu.be/t_3Os3xezlA",
+    image: "https://i.imgur.com/xHa3UFz.jpg",
+    title: "BOARSON AND EVASION LOOP CRAZY COMBOS! Playing with Cloaken - AceTheChase",
+    date: "September 24, 2020",
+  },
+  {
+    link: "https://www.instagram.com/p/CFerupUjeog/?igshid=17hzuya04bu0v",
+    image: "https://i.imgur.com/yQCnTuV.jpg",
+    title: "Fan Art - evelyn_s_arts",
+    date: "September 23, 2020",
+  },
+]
