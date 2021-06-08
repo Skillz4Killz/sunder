fa4a6be57738b532490cbed68fad84c88cc2b7e8
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-06-08 19:18:45 -0400

    Create semc.ts

---
+++ semc.ts
@@ -0,0 +1,66 @@
+export default [
+  {
+    name: "Zekent",
+    description: "Fine tunes the numbers so that he always wins.",
+    stats: [
+      "100% chance of victory!",
+      "You can earn everything that affects gameplay for free. However, you can pay to speed up your progress",
+      "Maxing out a single loadout should be fairly achieveable. It's not supposed to be fast, but it won't take forever.",
+    ],
+  },
+  {
+    name: "ta0",
+    description: "The bald head is very beautiful and inspiring.",
+    stats: [
+      "Feel free to ask questions in #deleted-channel",
+      "You can find all info about Secret Service program in #recruitment 😄",
+    ],
+  },
+  {
+    name: "Loquori",
+    description:
+      "Always wins because no one dares to attack. Those who attack are never heard from again as their data is deleted from the database.",
+    stats: [
+      "Don't sound kind. Be kind. The sound will take care of itself.",
+      "Modern software is built with more layers than a crepe cake.",
+      "Engineer shows up, and everyone quietly sneaks out of chat to go play a game. I see how it is. ;)",
+    ],
+  },
+  {
+    name: "cloaken",
+    description: "Risks breaking company NDA and share inside assets from the game!",
+    stats: [
+      "Never settle for the lesser evil!",
+      "Here are some more choices for your character. 🥰 (Don't tell @EdArt I showed you this...I'll get in big trouble...😬 )",
+      "We're crunching away, but wanted to pop in. Love all the world/story convos!",
+    ],
+  },
+  {
+    name: "Mejlis",
+    description: "Majestic as a genie granting your every wish !",
+    stats: [
+      "Welcome to our fam!",
+      "You guys givin me real nightmares right now",
+      "Once we know, you will be the first to know 😉",
+    ],
+  },
+  {
+    name: "anexi",
+    description: "If you let anexi take your flag, the game will be advertised and marketed!",
+    stats: [
+      "I still can't remember Nock all the time and called her in a conversation Baby Dragon. My daughter calls her Baby Dragon so she will be always our baby dragon.",
+      "Haha, your hype will die if you encounter Zekent in the game. The dude is a mechanical god. You would almost think he is cheating but he is not so super annoying to get slapped in the face with your weak eye-hand coordination",
+      "Marketing, Analytics, Live Ops, Monetization etc.",
+    ],
+  },
+  {
+    name: "Corpus",
+    description: "Just wins! Always Wins! The Goat Defender",
+    stats: [
+      "Enemies in close proximity to Corpus for more than 5 seconds fall into a dreaming trance for 2 seconds.",
+      "Corpus lets out a shriek that deals crystal damage in a short radius around him, applies a mortal wound to each target and refreshes the dream trance on enemies.",
+      "Corpus gouges the target with his spiraled horns and sticks to the target while it moves for 2.2 seconds.",
+      "Corpus plunges all sleeping enemies (globally) into a waking nightmare, causing them to lose control and attack the nearest ally for 3 seconds.",
+    ],
+  },
+]
