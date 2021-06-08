022bdaef4e2c268272fb1f9823132c6fc3360247
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-06-07 23:12:02 -0400

    Create news.ts

---
+++ news.ts
@@ -0,0 +1,44 @@
+export default [
+    {
+        link: "https://catalystblack.com/catalyst-black-content-creator-spotlight-9-18/",
+        image:"https://catalystblack.com/wp-content/uploads/2020/08/CC-Spotlight-Blog-1.png",
+        title: "CATALYST BLACK CONTENT CREATOR SPOTLIGHT 9/18",
+        date: "September 18, 2020",
+    },
+    {
+        link: "https://catalystblack.com/secret-service-0-3-patch-notes/",
+        image: "https://catalystblack.com/wp-content/uploads/2020/09/PatchNotes03_BlogHeader.png",
+        title: "SECRET SERVICE – 0.3 PATCH NOTES",
+        date: "September 9, 2020",
+    },
+    {
+        link: "https://catalystblack.com/content-creator-spotlight2/",
+        image: "https://catalystblack.com/wp-content/uploads/2020/08/CC-Spotlight-Blog-1.png",
+        title: "CATALYST BLACK CONTENT CREATOR SPOTLIGHT 8/28",
+        date: "August 28, 2020",
+    },
+    {
+        link: "https://catalystblack.com/content-creator-spotlight1/",
+        image: "https://catalystblack.com/wp-content/uploads/2020/08/CC-Spotlight-Blog.png",
+        title: "CATALYST BLACK CONTENT CREATOR SPOTLIGHT 8/21",
+        date: "August 21, 2020",
+    },
+    {
+        link: "https://catalystblack.com/watch-catalyst-black-content/",
+        image: "https://catalystblack.com/wp-content/uploads/2020/08/WatchContent.png",
+        title: "WATCH CATALYST BLACK CONTENT",
+        date: "August 17, 2020",
+    },
+    {
+        link: "https://catalystblack.com/the-secret-service/",
+        image: "https://catalystblack.com/wp-content/uploads/2020/07/SECRET-SERVICE-HEADER.png",
+        title: "THE SECRET SERVICE",
+        date: "July 23, 2020",
+    },
+    {
+        link: "https://catalystblack.com/progression-in-catalyst-black/",
+        image: "https://catalystblack.com/wp-content/uploads/2020/07/Banner-2.jpg",
+        title: "PROGRESSION IN CATALYST BLACK",
+        date: "July 16, 2020",
+    },
+]
