8d8cd19a8da23aafdeb599560461f5c156d98990
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-06-08 19:17:47 -0400

    Create powers.ts

---
+++ powers.ts
@@ -0,0 +1,322 @@
+export default [
+  {
+    name: "Leap Slam",
+    primal: "Sunder",
+    description: "You will only witness the first part.",
+    quote: "Sunder leaps into the air to slam back down dealing massive damage to an area.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Charge Time", value: "3.2s" },
+          { name: "Damage", value: "1600" },
+          { name: "Range", value: "8.6" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Charge Time", value: "3.2s" },
+          { name: "Damage", value: "1632" },
+          { name: "Range", value: "8.6" },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Breath of Rage",
+    primal: "Sunder",
+    description: "Channeling his primal fury, Sunder unleashes a focused beam of rage down the path in front of him.",
+    quote: "Pure rage, but looks like fire.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Charge Time", value: "4.4s" },
+          { name: "Damage Per Second", value: "2000" },
+          { name: "Range", value: "18" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Charge Time", value: "4.4s" },
+          { name: "Damage Per Second", value: "2040" },
+          { name: "Range", value: "18" },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Primal Roar",
+    primal: "Sunder",
+    description: "Issuing an otherworldly howl of anger, Sunder grows in size, speed, and power.",
+    quote: "If a sound could end you...",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Charge Time", value: "9.6s" },
+          { name: "Primary Damage Increase", value: "20%" },
+          { name: "Movespeed Strength", value: "40%" },
+          { name: "Duration", value: "6s" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Charge Time", value: "9.6s" },
+          { name: "Primary Damage Increase", value: "20.75%" },
+          { name: "Movespeed Strength", value: "41%" },
+          { name: "Duration", value: "6.07s" },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Deep Freeze",
+    primal: "Issia",
+    description:
+      "Issia hardens herself into an unbreakable form, emmanating cold damage to anything in the area around her.",
+    quote: "Mere inward reflection.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Charge Time", value: "8.6s" },
+          { name: "Damage Per Second", value: "2500" },
+          { name: "Maximum Radius", value: "7.2" },
+          { name: "Minimum Radius", value: "2.4" },
+          { name: "Duration", value: "3s" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Charge Time", value: "8.6s" },
+          { name: "Damage Per Second", value: "2550" },
+          { name: "Maximum Radius", value: "7.2" },
+          { name: "Minimum Radius", value: "2.4" },
+          { name: "Duration", value: "3s" },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Polar Vortex",
+    primal: "Issia",
+    description:
+      "Issia calls down a snow storm at a target location that deals periodic damage to anything caught inside.",
+    quote: "Disquiet that rattles mortality.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Charge Time", value: "6.5s" },
+          { name: "Damage Per Second", value: "1200" },
+          { name: "Effect Radius", value: "4.8" },
+          { name: "Duration", value: "3s" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Charge Time", value: "6.5s" },
+          { name: "Damage Per Second", value: "1224" },
+          { name: "Effect Radius", value: "4.8" },
+          { name: "Duration", value: "3s" },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Wing Sweep",
+    primal: "Issia",
+    description:
+      "Issia strikes out with her wings, issuing a powerful gust of wind that knocks back anything in the area in front of her.",
+    quote: "Ambivilent to your existence...",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Charge Time", value: "6.5s" },
+          { name: "Damage Per Second", value: "1200" },
+          { name: "Effect Radius", value: "4.8" },
+          { name: "Duration", value: "3s" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Charge Time", value: "6.5s" },
+          { name: "Damage Per Second", value: "1224" },
+          { name: "Effect Radius", value: "4.8" },
+          { name: "Duration", value: "3s" },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Thunderbolt",
+    primal: "Torden",
+    description: "Torden calls down a bolt of lightning, dealing damage to a target area.",
+    quote: "Can strike twice in the same location.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Max Charges", value: "3" },
+          { name: "Charge Time", value: "3.8s" },
+          { name: "Damage", value: "820" },
+          { name: "Range", value: "13.2" },
+          { name: "Effect Radius", value: "3.4" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Max Charges", value: "3" },
+          { name: "Charge Time", value: "3.8s" },
+          { name: "Damage", value: "836" },
+          { name: "Range", value: "13.2" },
+          { name: "Effect Radius", value: "3.4" },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Reckoning",
+    primal: "Torden",
+    description:
+      "Torden throws his hammer, dealing light damage to the path in front of him. Reactivating this ability during the throw will teleport Torden to his hammer, and finish with a powerful hammer smash.",
+    quote: "He can move if he wants to.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Charge Time", value: "5.8s" },
+          { name: "Damage", value: "740" },
+          { name: "Range", value: "12" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Charge Time", value: "5.8s" },
+          { name: "Damage", value: "754" },
+          { name: "Range", value: "12" },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Guardian",
+    primal: "Torden",
+    description: "Torden targets an area, granting all allies inside a large shield.",
+    quote: "A happy soul is the best shield.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Charge Time", value: "4.4s" },
+          { name: "Shield Strength", value: "1200" },
+          { name: "Range", value: "12.8" },
+          { name: "Effect Radius", value: "6" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Charge Time", value: "4.4s" },
+          { name: "Shield Strength", value: "1224" },
+          { name: "Range", value: "12.8" },
+          { name: "Effect Radius", value: "6" },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Faster!",
+    primal: "Nock",
+    description: "Nock ignites with primal power, increasing her movespeed and constantly burning all nearby enemies.",
+    quote: "Don't show interest. Walk away. Run if you have to.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Charge Time", value: "10.2s" },
+          { name: "Damage Per Second", value: "1250" },
+          { name: "Range", value: "9" },
+          { name: "Duration", value: "4s" },
+          { name: "Movespeed Strength", value: "30%" },
+          { name: "Effect Radius", value: "3.4" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Charge Time", value: "10.2s" },
+          { name: "Damage Per Second", value: "1275" },
+          { name: "Range", value: "9" },
+          { name: "Duration", value: "4.1s" },
+          { name: "Movespeed Strength", value: "30.25%" },
+          { name: "Effect Radius", value: "3.4" },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Poof!",
+    primal: "Nock",
+    description:
+      "Nock teleports to a target location, dealing damage to nearby enemies at the beginnin and end of her transition.",
+    quote: "It is hard to see this coming.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Charge Time", value: "8.2.4s" },
+          { name: "Damage", value: "750" },
+          { name: "Range", value: "14" },
+          { name: "Effect Radius", value: "3.6" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Charge Time", value: "8.2.4s" },
+          { name: "Damage", value: "765" },
+          { name: "Range", value: "14" },
+          { name: "Effect Radius", value: "3.6" },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Watch This!",
+    primal: "Nock",
+    description:
+      "Nock shoots out a small ball of primal energy. When it hits an enemy or expires, a massive explosion damages everything in the area.",
+    quote: "Don't listen to her.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Charge Time", value: "5.2s" },
+          { name: "Damage", value: "1200" },
+          { name: "Range", value: "14" },
+          { name: "Effect Radius", value: "5.4" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Charge Time", value: "5.2s" },
+          { name: "Damage", value: "1224" },
+          { name: "Range", value: "14" },
+          { name: "Effect Radius", value: "5.4" },
+        ],
+      },
+    ],
+  },
+]
