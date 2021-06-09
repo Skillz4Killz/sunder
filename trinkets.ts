72b48cdfa18f6b1c85ed93117739f08213ac6c00
isorod <isorod@gmail.com>
2021-06-08 21:06:13 -0400

    Update trinkets.ts

--- trinkets.ts
+++ trinkets.ts
@@ -7,17 +7,17 @@ export default [
       {
         level: 1,
         stats: [
-          { name: "Health Max", value: "40" },
-          { name: "Shield Duration", value: "1s" },
-          { name: "Shield Strength", value: "225" },
+          { name: "Health Max", value: "236" },
+          { name: "Shield Duration", value: "1.8s" },
+          { name: "Shield Strength", value: "350" },
         ],
       },
       {
         level: 2,
         stats: [
-          { name: "Health Max", value: "50.8" },
-          { name: "Shield Duration", value: "1.01s" },
-          { name: "Shield Strength", value: "230" },
+          { name: "Health Max", value: "261" },
+          { name: "Shield Duration", value: "1.8s" },
+          { name: "Shield Strength", value: "357" },
         ],
       },
     ],
@@ -31,14 +31,14 @@ export default [
       {
         level: 1,
         stats: [
-          { name: "Health Max", value: "100" },
+          { name: "Health Max", value: "200" },
           { name: "Primal Charge", value: "125%" },
         ],
       },
       {
         level: 2,
         stats: [
-          { name: "Health Max", value: "112" },
+          { name: "Health Max", value: "224" },
           { name: "Primal Charge", value: "127.5%" },
         ],
       },
@@ -53,15 +53,15 @@ export default [
       {
         level: 1,
         stats: [
-          { name: "Health Max", value: "60" },
-          { name: "Primal Damage Reduction", value: "40%" },
+          { name: "Health Max", value: "120" },
+          { name: "Primal Damage Reduction", value: "65%" },
         ],
       },
       {
         level: 2,
         stats: [
-          { name: "Health Max", value: "71.2" },
-          { name: "Primal Damage Reduction", value: "41%" },
+          { name: "Health Max", value: "142" },
+          { name: "Primal Damage Reduction", value: "65%" },
         ],
       },
     ],
@@ -75,14 +75,14 @@ export default [
       {
         level: 1,
         stats: [
-          { name: "Health Max", value: "80" },
+          { name: "Health Max", value: "160" },
           { name: "Effect Cooldown", value: "4s" },
         ],
       },
       {
         level: 2,
         stats: [
-          { name: "Health Max", value: "91.6" },
+          { name: "Health Max", value: "183" },
           { name: "Effect Cooldown", value: "3.85s" },
         ],
       },
@@ -98,14 +98,14 @@ export default [
         level: 1,
         stats: [
           { name: "Health Max", value: "80" },
-          { name: "Heal Strength", value: "175" },
+          { name: "Heal Strength", value: "100" },
         ],
       },
       {
         level: 2,
         stats: [
-          { name: "Health Max", value: "91.6" },
-          { name: "Heal Strength", value: "179" },
+          { name: "Health Max", value: "102" },
+          { name: "Heal Strength", value: "102" },
         ],
       },
     ],
@@ -113,24 +113,24 @@ export default [
   },
   {
     name: "Empathy Band",
-    description: "You and nearby allies gain a shield based on your max health at the end of your dash.",
+    description: "You and nearby allies gain a shield at the end of your dodge.",
     quote: "If you want to lead, you'll have to serve.",
     stats: [
       {
         level: 1,
         stats: [
-          { name: "Health Max", value: "40" },
-          { name: "Health to Shield Ratio", value: "6%" },
-          { name: "Shield Duration", value: "1s" },
+          { name: "Health Max", value: "80" },
+          { name: "Shield Strength", value: "280" },
+          { name: "Shield Duration", value: "1.6s" },
           { name: "Effect Radius", value: "5.2" },
         ],
       },
       {
         level: 2,
         stats: [
-          { name: "Health Max", value: "50.8" },
-          { name: "Health to Shield Ratio", value: "6.1%" },
-          { name: "Shield Duration", value: "1.01s" },
+          { name: "Health Max", value: "102" },
+          { name: "Shield Strength", value: "285" },
+          { name: "Shield Duration", value: "1.6s" },
           { name: "Effect Radius", value: "5.2" },
         ],
       },
@@ -145,8 +145,8 @@ export default [
       {
         level: 1,
         stats: [
-          { name: "Health Max", value: "60" },
-          { name: "Delay Before Trigger", value: "4s" },
+          { name: "Health Max", value: "120" },
+          { name: "Delay Before Trigger", value: "2s" },
           { name: "Movespeed Duration", value: "2.4s" },
           { name: "Movespeed Strength", value: "50%" },
         ],
@@ -154,10 +154,10 @@ export default [
       {
         level: 2,
         stats: [
-          { name: "Health Max", value: "71.2" },
-          { name: "Delay Before Trigger", value: "3.95s" },
-          { name: "Movespeed Duration", value: "2.43s" },
-          { name: "Movespeed Strength", value: "50.25%" },
+          { name: "Health Max", value: "142" },
+          { name: "Delay Before Trigger", value: "1.96s" },
+          { name: "Movespeed Duration", value: "2.44s" },
+          { name: "Movespeed Strength", value: "50%" },
         ],
       },
     ],
@@ -171,7 +171,7 @@ export default [
       {
         level: 1,
         stats: [
-          { name: "Health Max", value: "80" },
+          { name: "Health Max", value: "160" },
           { name: "Bonus Damage Duration", value: "1.8s" },
           { name: "Bonus Damage Strength", value: "5%" },
           { name: "Effect Radius", value: "4.2" },
@@ -180,7 +180,7 @@ export default [
       {
         level: 2,
         stats: [
-          { name: "Health Max", value: "91.6" },
+          { name: "Health Max", value: "183" },
           { name: "Bonus Damage Duration", value: "1.83s" },
           { name: "Bonus Damage Strength", value: "5.1%" },
           { name: "Effect Radius", value: "4.2" },
@@ -191,25 +191,23 @@ export default [
   },
   {
     name: "Bracelet of the Firstborn",
-    description: "You and your allies gain a shield based on your max health when you activate your primal ability.",
+    description: "You and your allies gain invulnerability for a short time when you transform.",
     quote: "Unmerited favor beyond your comprehension.",
     stats: [
       {
         level: 1,
         stats: [
-          { name: "Health Max", value: "120" },
-          { name: "Health to Shield Ratio", value: "50%" },
-          { name: "Shield Duration", value: "2.8s" },
-          { name: "Effect Radius", value: "6.8" },
+          { name: "Health Max", value: "320" },
+          { name: "Shield Duration", value: "1.6s" },
+          { name: "Effect Radius", value: "7.2" },
         ],
       },
       {
         level: 2,
         stats: [
-          { name: "Health Max", value: "132" },
-          { name: "Health to Shield Ratio", value: "50.75%" },
-          { name: "Shield Duration", value: "2.83s" },
-          { name: "Effect Radius", value: "6.8" },
+          { name: "Health Max", value: "346" },
+          { name: "Shield Duration", value: "1.62s" },
+          { name: "Effect Radius", value: "7.2" },
         ],
       },
     ],
@@ -223,15 +221,15 @@ export default [
       {
         level: 1,
         stats: [
-          { name: "Health Max", value: "140" },
-          { name: "Effect Cooldown", value: "45%" },
+          { name: "Health Max", value: "280" },
+          { name: "Bonus Charge", value: "45%" },
         ],
       },
       {
         level: 2,
         stats: [
-          { name: "Health Max", value: "153" },
-          { name: "Effect Cooldown", value: "46%" },
+          { name: "Health Max", value: "306" },
+          { name: "Bonus Charge", value: "46%" },
         ],
       },
     ],
@@ -245,15 +243,15 @@ export default [
       {
         level: 1,
         stats: [
-          { name: "Health Max", value: "60" },
-          { name: "Heavy Ammo", value: "80%" },
+          { name: "Health Max", value: "120" },
+          { name: "Heavy Ammo", value: "65%" },
         ],
       },
       {
         level: 2,
         stats: [
-          { name: "Health Max", value: "71.2" },
-          { name: "Heavy Ammo", value: "82%" },
+          { name: "Health Max", value: "142" },
+          { name: "Heavy Ammo", value: "66.25%" },
         ],
       },
     ],
@@ -267,17 +265,17 @@ export default [
       {
         level: 1,
         stats: [
-          { name: "Health Max", value: "40" },
-          { name: "Movespeed Duration", value: "2s" },
-          { name: "Movespeed Strength", value: "50%" },
+          { name: "Health Max", value: "80" },
+          { name: "Movespeed Duration", value: "1s" },
+          { name: "Movespeed Strength", value: "25%" },
         ],
       },
       {
         level: 2,
         stats: [
-          { name: "Health Max", value: "50.8" },
-          { name: "Movespeed Duration", value: "2.04s" },
-          { name: "Movespeed Strength", value: "51%" },
+          { name: "Health Max", value: "102" },
+          { name: "Movespeed Duration", value: "1" },
+          { name: "Movespeed Strength", value: "25.5%" },
         ],
       },
     ],
@@ -291,15 +289,15 @@ export default [
       {
         level: 1,
         stats: [
-          { name: "Health Max", value: "83.2" },
-          { name: "All Damage", value: "11%" },
+          { name: "Health Max", value: "80" },
+          { name: "All Damage", value: "10%" },
         ],
       },
       {
         level: 2,
         stats: [
-          { name: "Health Max", value: "94" },
-          { name: "All Damage", value: "11.2%" },
+          { name: "Health Max", value: "102" },
+          { name: "All Damage", value: "10.2%" },
         ],
       },
     ],
@@ -313,18 +311,116 @@ export default [
       {
         level: 1,
         stats: [
-          { name: "Health Max", value: "140" },
-          { name: "Bonus Duration", value: "10%" },
+          { name: "Health Max", value: "280" },
+          { name: "Bonus Duration", value: "10s" },
         ],
       },
       {
         level: 2,
         stats: [
-          { name: "Health Max", value: "153" },
-          { name: "Bonus Duration", value: "10.4%" },
+          { name: "Health Max", value: "306" },
+          { name: "Bonus Duration", value: "10.2s" },
         ],
       },
     ],
     banner: "Vanguard",
+  },
+   {
+    name: "Compass of the Ascendant",
+    description: "Your dodge is refunded if it ends near a catalyte crystal.",
+    quote: "There is incredible value and power inherent in catalyte, but to what end? It depends on who you ask. \n \n With this took, you now have the ability to find catalyte and benefit from it, even if only in this small way.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Health Max", value: "320" },
+          { name: "Effect Cooldown ", value: "4s" },
+          { name: "Search Radius ", value: "2.8" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Health Max", value: "346" },
+          { name: "Effect Cooldown ", value: "3.85s" },
+          { name: "Search Radius ", value: "2.8" },
+        ],
+      },
+    ],
+    banner: "Seeker",
+  },
+  {
+    name: "Clemency Vial",
+    description: "Upon dropping below half health, become invulnerable for a brief time.",
+    quote: "Alchemy is not a common practice amongst the Kyrians. In particular, there are few knowledgable of embuing catalytic properties into ingestible forms.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Health Max", value: "120" },
+          { name: "Invulnerability Duration", value: "0.8s" },
+          { name: "Max Health Trigger", value: "50%" },
+          { name: "Effect Cooldown", value: "60s" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Health Max", value: "142" },
+          { name: "Invulnerability Duration", value: "0.8s" },
+          { name: "Max Health Trigger", value: "50%" },
+          { name: "Effect Cooldown", value: "59.2s" },
+             ],
+      },
+    ],
+    banner: "Vanguard",
+  },
+  {
+    name: "Kindred Clasp",
+    description: "Gain increased movespeed when near an allied primal.",
+    quote: "Bonded to a place beyond the Veil, you yearn to see it with your own eyes.",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Health Max", value: "360" },
+          { name: "Movespeed Strength", value: "50%" },
+          { name: "Search Radius ", value: "5.8" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Health Max", value: "387" },
+          { name: "Movespeed Strength", value: "50.5%" },
+          { name: "Search Radius ", value: "5.87" },
+         ],
+      },
+    ],
+    banner: "Primalist",
+  },
+    {
+    name: "Yurtak Wraps",
+    description: "Gain increased movespeed if you have not fired a weapon for a short time. This buff persists until you fire again.",
+    quote: "Yurtak fighters focus on their deadly use of stealth and speed. \n \n 'A surprise attack is twice the offense.' ",
+    stats: [
+      {
+        level: 1,
+        stats: [
+          { name: "Health Max", value: "160" },
+          { name: "Movespeed Strength", value: "30%" },
+          { name: "Effect Cooldown", value: "6s" },
+        ],
+      },
+      {
+        level: 2,
+        stats: [
+          { name: "Health Max", value: "183" },
+          { name: "Movespeed Strength", value: "30.5%" },
+          { name: "Effect Cooldown", value: "5.9s" },
+          ],
+      },
+    ],
+    banner: "Ranger",
   },
 ]
